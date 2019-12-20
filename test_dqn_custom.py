78d9d53b8fd4095fcbb6ec7d61386e515ed0f5d5
Jorge Huete <jorgehuetes@gmail.com>
2019-12-20 13:03:35 +0100

    Increased episode size

--- test_dqn_custom.py
+++ test_dqn_custom.py
@@ -10,7 +10,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, Activation, MaxPooling2D, Flatten
 from test_1_tank_controls.environment import Environment
 
-EPISODES = 2_000
+EPISODES = 12_000
 REPLAY_MEMORY_SIZE = 2_000
 MIN_REPLAY_MEMORY_SIZE = 1_000
 MODEL_NAME = "sequential"
@@ -26,6 +26,10 @@ EPSILON_DECAY = 0.99975
 MIN_EPSILON = 0.001
 
 env = Environment()
+
+AGGREGATE_STATS_EVERY = 50  # episodes
+
+MIN_REWARD = -500  # For model save
 
 # Own Tensorboard class
 class ModifiedTensorBoard(TensorBoard):
@@ -58,7 +62,7 @@ class ModifiedTensorBoard(TensorBoard):
     # Creates writer, writes custom metrics and closes writer
     def update_stats(self, **stats):
         pass
-        #self._write_logs(stats, self.step)
+        # self._write_logs(stats, self.step)
 
 
 class DQNAgent:
@@ -155,7 +159,6 @@ class DQNAgent:
             self.target_update_counter = 0
 
 
-
 if __name__ == "__main__":
     agent = DQNAgent()
 
@@ -172,9 +175,18 @@ if __name__ == "__main__":
 
     for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
 
+        # Update tensorboard step every episode
+        agent.tensorboard.step = episode
+
+        # Restarting episode - reset episode reward and step number
+        episode_reward = 0
+        step = 1
 
         done = False
         env, reward, current_state, done = env.reset()
+
+        if (episode == EPISODES):
+            env.render()
 
         while not done:
             # This part stays mostly the same, the change is to query a model for Q values
@@ -185,11 +197,31 @@ if __name__ == "__main__":
                 # Get random action
                 action = np.random.randint(0, env.ACTION_SPACE_SIZE)
 
-            reward, current_state, done = env.step(action)
+            reward, new_state, done = env.step(action)
+            # Transform new continous state to new discrete state and count reward
+            episode_reward += reward
 
+            '''if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+                env.render()'''
+
+            # Every step we update replay memory and train main network
+            agent.update_replay_memory((current_state, action, reward, new_state, done))
+            agent.train(done, step)
+            current_state = new_state
+            step += 1
+
+            # Append episode reward to a list and log stats (every given number of episodes)
+            ep_rewards.append(episode_reward)
+            if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+                average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+                min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+                max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+                agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward,
+                                               epsilon=epsilon)
+
+                # Save model, but only when min reward is greater or equal a set value
+                if average_reward >= MIN_REWARD:
+                    agent.model.save(
+                        f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
 
     print("Done!")
-
-
-
-
