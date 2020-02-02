09e066e334d7b52962a09a8a1d32b0277998ed4e
Jorge <jorgehuetes@gmail.com>
2020-02-01 21:56:12 -0500

    Iteration over learning rates

--- multithreading_test.py
+++ multithreading_test.py
@@ -1,69 +1,75 @@
 import threading
 
 import numpy as np
-from matplotlib import pyplot
 from tqdm import tqdm
 from environment import Environment
 from GUI_new_model_trining import DQNAgent
 import config as cfg
+import math
 
 if __name__ == '__main__':
-    lr = float(input("LR: "))
-    print(lr)
-    env = Environment()
-    agent = DQNAgent(lr)
-    episode_reward_history = []
-    average_reward_history = []
-    epsilon = cfg.DEF_EPSILON
+    ini_lr = float(input("INI LR: "))
+    end_lr = float(input("END_LR: "))
+    N = int(input("N: "))
+    step = math.fabs(ini_lr-end_lr)/N
+    for i in range(N+1):
+        lr = ini_lr+i*step
+        print(lr)
 
-    for episode in tqdm(range(400), unit='sims'):
+        env = Environment()
+        agent = DQNAgent(lr)
+        episode_reward_history = []
+        average_reward_history = []
+        epsilon = cfg.DEF_EPSILON
 
-        # Episode initialization
-        episode_reward = 0
-        reward, state, done = env.reset()
-        done = False
-        step = 0
+        for episode in tqdm(range(6000), unit='sims'):
 
-        while not done:
-            step += 1
+            # Episode initialization
+            episode_reward = 0
+            reward, state, done = env.reset()
+            done = False
+            step = 0
 
-            # Make a decision based on epsilon
-            if np.random.random() > epsilon:
-                # Action/Prediction generation
-                tmp_state = np.array(state).reshape(-1, env.STATE_SIZE)
-                prediction = agent.model.predict(tmp_state)
-                action = np.argmax(prediction)
-            else:
-                # Get random action
-                action = np.random.randint(0, env.ACTION_SIZE)
+            while not done:
+                step += 1
 
-            # New environment state
-            reward, new_state, done = env.step(action)
-            episode_reward += reward
+                # Make a decision based on epsilon
+                if np.random.random() > epsilon:
+                    # Action/Prediction generation
+                    tmp_state = np.array(state).reshape(-1, env.STATE_SIZE)
+                    prediction = agent.model.predict(tmp_state)
+                    action = np.argmax(prediction)
+                else:
+                    # Get random action
+                    action = np.random.randint(0, env.ACTION_SIZE)
 
-            # Update memory and training
-            agent.update_replay_memory((state, action, reward, new_state, done))
-            if not episode % cfg.TRAIN_EVERY:
-                agent.train(done)
+                # New environment state
+                reward, new_state, done = env.step(action)
+                episode_reward += reward
 
-            state = new_state
+                # Update memory and training
+                agent.update_replay_memory((state, action, reward, new_state, done))
+                if not episode % cfg.TRAIN_EVERY:
+                    agent.train(done)
 
-        episode_reward_history.append(episode_reward)
+                state = new_state
 
-        # Decay epsilon
-        if epsilon > cfg.MIN_EPSILON:
-            epsilon *= cfg.EPSILON_DECAY
-            epsilon = max(cfg.MIN_EPSILON, epsilon)
+            episode_reward_history.append(episode_reward)
 
-        if not episode % cfg.AGGREGATE_EVERY or episode == 1:
-            avg = sum(episode_reward_history) / len(episode_reward_history)
-            print("epsilon: ", epsilon, " avg: ", avg)
-            average_reward_history.append(avg)
-            episode_reward_history = []
+            # Decay epsilon
+            if epsilon > cfg.MIN_EPSILON:
+                epsilon *= cfg.EPSILON_DECAY
+                epsilon = max(cfg.MIN_EPSILON, epsilon)
 
-    print(average_reward_history)
-    myfile = open("model_lr_"+str(lr)+".txt", "w")
-    for i in average_reward_history:
-        myfile.write(str(i) + '\n')
+            if not episode % cfg.AGGREGATE_EVERY or episode == 1:
+                avg = sum(episode_reward_history) / len(episode_reward_history)
+                print("epsilon: ", epsilon, " avg: ", avg)
+                average_reward_history.append(avg)
+                episode_reward_history = []
 
-    myfile.close()
+        print(average_reward_history)
+        myfile = open("model_lr_"+str(lr)+".txt", "w")
+        for i in average_reward_history:
+            myfile.write(str(i) + '\n')
+
+        myfile.close()
