9b7cb6707e54a49ebc0ddca1a51f46e257350a68
Jorge Huete <jorgehuetes@gmail.com>
2020-03-17 17:54:16 +0100

    Added Hough/DBSCAN wall detection

--- preprocessing/wallsCV.py
+++ preprocessing/wallsCV.py
@@ -1,35 +1,101 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
 from preprocessing.world import Wall
 import math
 
 
 def fix_line(x1, y1, x2, y2, x_shape, y_shape):
+    tan = (y2 - y1) / (x2 - x1)
+    i_tan = (x2 - x1) / (y2 - y1)
     if x1 < 0:
-        y1 = int(y1 - x1 * ((y2 - y1) / (x2 - x1)))
+        y1 = int(y1 - x1 * tan)
         x1 = 0
 
+    if x1 > y_shape:
+        y1 = int(y1 - (x1 - y_shape) * i_tan)
+        x1 = y_shape
+
+    if x2 < 0:
+        y2 = int(y2 - x2 * tan)
+        x2 = 0
+
     if x2 > y_shape:
-        y2 = int(y2 - (x2 - y_shape) * ((y2 - y1) / (x2 - x1)))
+        y2 = int(y2 - (x2 - y_shape) * tan)
         x2 = y_shape
 
+    if y1 < 0:
+        x1 = int(x1 - y1 * i_tan)
+        y1 = 0
+
     if y1 > x_shape:
-        x1 = int(x1 - (y1 - x_shape) * ((x2 - x1) / (y2 - y1)))
+        x1 = int(x1 - (y1 - x_shape) * i_tan)
         y1 = x_shape
 
     if y2 < 0:
-        x2 = int(x2 - y2 * ((y2 - y1) / (x2 - x1)))
+        x2 = int(x2 - y2 * i_tan)
         y2 = 0
 
     if y2 > x_shape:
-        x2 = int(x2 - (y2 - x_shape) * ((x2 - x1) / (y2 - y1)))
+        x2 = int(x2 - (y2 - x_shape) * i_tan)
         y2 = x_shape
 
     return x1, y1, x2, y2
 
 
-def doHoughTransform(x, y, resolution_div, max_distance):
+def remove_redundant_lines(lines):
+    lines_inner = np.array([x[0] for x in lines])
+    clustering = DBSCAN(eps=12, min_samples=1).fit(lines_inner)
+
+    n_clusters = len(set(clustering.labels_))
+    clusters = [lines_inner[clustering.labels_ == i] for i in range(n_clusters)]
+
+    clean_lines = []
+    for cluster in clusters:
+        rho = np.average(np.transpose(cluster)[0])
+        theta = np.average(np.transpose(cluster)[1])
+
+        clean_lines.append([[rho, theta]])
+
+    return clean_lines
+
+
+def distance_point_line(lp1, lp2, p):
+    x1 = lp1[0]
+    y1 = lp1[1]
+    x2 = lp2[0]
+    y2 = lp2[1]
+
+    x0 = p[0]
+    y0 = p[1]
+
+    delta_l = math.sqrt(math.pow(y2 - y1, 2) + math.pow(x2 - x1, 2))
+    delta_p = math.fabs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
+
+    return delta_p / delta_l
+
+
+def remove_wall_points(x, y, walls):
+    non_walls_x = []
+    non_walls_y = []
+
+    for p_i in range(len(x)):
+        dmin = 99999
+        for wall in walls:
+
+            d = distance_point_line((wall.start_x, wall.start_y),
+                                    (wall.end_x, wall.end_y),
+                                    (x[p_i], y[p_i]))
+            if d < dmin:
+                dmin = d
+        if dmin > 70:
+            non_walls_x.append(x[p_i])
+            non_walls_y.append(y[p_i])
+
+    return non_walls_x, non_walls_y
+
+
+def doHoughTransform(x, y, resolution_div):
     walls = []
 
     x = np.array(x).astype(int)
@@ -41,8 +107,6 @@ def doHoughTransform(x, y, resolution_div, max_distance):
     x_shape = (np.max(x) - np.min(x)) // resolution_div
     y_shape = (np.max(y) - np.min(y)) // resolution_div
 
-    print(x_shape, y_shape)
-
     im = np.zeros((x_shape + 1, y_shape + 1))
     for p in range(len(x)):
         pixel_x = (x[p] - offset_x) // resolution_div
@@ -51,8 +115,9 @@ def doHoughTransform(x, y, resolution_div, max_distance):
 
     im = np.uint8(im)
 
-    lines = cv2.HoughLines(np.uint8(im), 1, np.pi / 200, 80)
-    print(len(lines))
+    lines = cv2.HoughLines(np.uint8(im), 1, np.pi / 600, 30)
+    lines = remove_redundant_lines(lines)
+
     for line in lines:
         rho, theta = line[0]
         a = np.cos(theta)
@@ -67,31 +132,5 @@ def doHoughTransform(x, y, resolution_div, max_distance):
         x1, y1, x2, y2 = fix_line(x1, y1, x2, y2, x_shape, y_shape)
 
         walls.append(Wall((y1, x1), (y2, x2)))
-        # cv2.line(z, (x1,y1),(x2,y2), 255)
-        # cv2.line(z, (-100,0), (x_shape//3, y_shape//2),  127)
 
     return walls, offset_x, offset_y
-
-
-"""fig = plt.figure()
-ax = fig.gca()
-plt.axis([-max_distance, max_distance, -max_distance, max_distance])
-for l in labels:
-    x = []
-    y = []
-    for p in range(len(points)):
-        if clustering.labels_[p] == l:
-            x.append(points[p][0])
-            y.append(points[p][1])
-
-    lg = linear_model.LinearRegression().fit(np.array(x).reshape(-1, 1), np.array(y).reshape(-1, 1))
-    if lg.score(np.array(x).reshape(-1, 1), np.array(y).reshape(-1, 1)) < 0.5:
-        clustering = OPTICS(min_samples=0.10, max_eps=300, xi=0.01).fit(np.array([x ,y]))
-
-
-    ax.scatter(x, y, s=1)
-    ax.scatter(x, lg.predict(np.array(x).reshape(-1, 1)))
-
-ax.set_aspect('equal')
-plt.show()
-"""
--- preprocessing/world.py
+++ preprocessing/world.py
@@ -37,25 +37,38 @@ if __name__ == "__main__":
     dsize = driver.start_scan_express()
 
     max_distance = 3500
+    samples = 500
 
-    points, x, y = driver.get_point_cloud(dsize, 900, max_distance)
+    points, x, y = driver.get_point_cloud(dsize, samples, max_distance)
     resolution_div = 8
+
     t = time.time()
-    walls, offset_x, offset_y = doHoughTransform(x, y, resolution_div, max_distance)
-    print(time.time()-t)
+    walls, offset_x, offset_y = doHoughTransform(x, y, resolution_div)
+    print(time.time() - t)
 
     fig = plt.figure()
     ax = fig.gca()
     plt.axis([-max_distance, max_distance, -max_distance, max_distance])
 
-    ax.scatter(x, y, s=1)
+
     for wall in walls:
         y1 = wall.start_y * resolution_div + offset_y
         y2 = wall.end_y * resolution_div + offset_y
         x1 = wall.start_x * resolution_div + offset_x
         x2 = wall.end_x * resolution_div + offset_x
-        plt.plot([x1, x2],
-                 [y1, y2], 'k-', lw=2)
+
+        wall.start_x = x1
+        wall.start_y = y1
+        wall.end_x = x2
+        wall.end_y = y2
+
+    for wall in walls:
+        plt.plot([wall.start_x, wall.end_x],
+                 [wall.start_y, wall.end_y], 'k-', lw=2, alpha=0.66)
+
+
+    x,y = remove_wall_points(x, y, walls)
+    ax.scatter(x, y, s=1)
 
     ax.set_aspect('equal')
     plt.ioff()
