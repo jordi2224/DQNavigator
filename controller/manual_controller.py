4228271a9020f52d330d90a5bdcaafc4aa7d020b
Jorge Huete <jorgehuetes@gmail.com>
2020-04-13 20:08:03 +0200

    Manual cont with decoder

--- controller/manual_controller.py
+++ controller/manual_controller.py
@@ -6,38 +6,48 @@ from controller.comm_definitions import *
 TCP_IP = '192.168.1.177'
 TCP_PORT = 420
 
-
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    msg = START_STR + "{\"type\":\"REQUEST\", \"request\": \"GET_SCAN\"}" + END_STR
-    s.send(msg.encode('utf-8'))
-    s.settimeout(0.1)
+    request = START_STR + "{\"type\":\"REQUEST\", \"request\": \"GET_SCAN\"}" + END_STR
+    s.send(request.encode('utf-8'))
+    s.settimeout(0.01)
     buff = ''
-    while 1:
 
+    counter = 0
+    while 1:
         try:
-            buff += s.recv(5000).decode('utf-8')
+            buff += s.recv(4096).decode('utf-8')
         except:
             pass
+        counter += 1
+
+        if not counter % 50:
+            counter = 0
+            s.send(request.encode('utf-8'))
+            print("Requested data")
 
         if is_complete(buff):
-            print(buff)
             msg, buff = receive_msg(buff)
             msg = parse(msg)
-            print("msg")
+            print(msg)
 
 
-        if keyboard.is_pressed('w') and not keyboard.is_pressed('s') and not keyboard.is_pressed('a') and not keyboard.is_pressed('d'):
+
+        if keyboard.is_pressed('w') and not keyboard.is_pressed('s') and not keyboard.is_pressed(
+                'a') and not keyboard.is_pressed('d'):
             msg = START_STR + "{\"type\":\"MANUAL_MOVE_ORDER\", \"direction\": \"FWD\"}" + END_STR
             s.send(msg.encode('utf-8'))
-        elif not keyboard.is_pressed('w') and keyboard.is_pressed('s') and not keyboard.is_pressed('a') and not keyboard.is_pressed('d'):
+        elif not keyboard.is_pressed('w') and keyboard.is_pressed('s') and not keyboard.is_pressed(
+                'a') and not keyboard.is_pressed('d'):
             msg = START_STR + "{\"type\":\"MANUAL_MOVE_ORDER\", \"direction\": \"BWD\"}" + END_STR
             s.send(msg.encode('utf-8'))
-        elif not keyboard.is_pressed('w') and not keyboard.is_pressed('s') and keyboard.is_pressed('a') and not keyboard.is_pressed('d'):
+        elif not keyboard.is_pressed('w') and not keyboard.is_pressed('s') and keyboard.is_pressed(
+                'a') and not keyboard.is_pressed('d'):
             msg = START_STR + "{\"type\":\"MANUAL_MOVE_ORDER\", \"direction\": \"RIGHT\"}" + END_STR
             s.send(msg.encode('utf-8'))
-        elif not keyboard.is_pressed('w') and not keyboard.is_pressed('s') and not keyboard.is_pressed('a') and keyboard.is_pressed('d'):
+        elif not keyboard.is_pressed('w') and not keyboard.is_pressed('s') and not keyboard.is_pressed(
+                'a') and keyboard.is_pressed('d'):
             msg = START_STR + "{\"type\":\"MANUAL_MOVE_ORDER\", \"direction\": \"LEFT\"}" + END_STR
             s.send(msg.encode('utf-8'))
 
