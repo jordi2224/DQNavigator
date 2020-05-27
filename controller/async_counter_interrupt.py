ee8200daf04b1ae1ed33e3e9496fae45a5917ea4
Jorge Huete <jorgehuetes@gmail.com>
2020-05-27 23:18:27 +0200

    Fixing interrupt based tracking

--- controller/async_counter_interrupt.py
+++ controller/async_counter_interrupt.py
@@ -5,43 +5,37 @@ counter_L = 0
 counter_R = 0
 
 
-def event_left_rising(sig):
+def event_left(sig):
     global counter_L
-    if GPIO.input(LTH2):
-        counter_L += 1
+    if GPIO.input(LTH1):
+        if GPIO.input(LTH2):
+            counter_L += 1
+        else:
+            counter_L -= 1
     else:
-        counter_L -= 1
+        if GPIO.input(LTH2):
+            counter_L -= 1
+        else:
+            counter_L += 1
 
 
-def event_left_falling(sig):
-    global counter_L
-    if GPIO.input(LTH2):
-        counter_L -= 1
-    else:
-        counter_L += 1
-
-
-def event_right_rising(sig):
+def event_right(sig):
     global counter_R
-    if GPIO.input(RTH2):
-        counter_R += 1
+    if GPIO.input(RTH1):
+        if GPIO.input(RTH2):
+            counter_R += 1
+        else:
+            counter_R -= 1
     else:
-        counter_R -= 1
+        if GPIO.input(RTH2):
+            counter_R -= 1
+        else:
+            counter_R += 1
 
 
-def event_right_falling(sig):
-    global counter_R
-    if GPIO.input(RTH2):
-        counter_R -= 1
-    else:
-        counter_R += 1
+GPIO.add_event_detect(LTH1, GPIO.BOTH, callback=event_left)
 
-
-GPIO.add_event_detect(LTH1, GPIO.RISING, callback=event_left_rising)
-GPIO.add_event_detect(LTH1, GPIO.FALLING, callback=event_left_falling)
-
-GPIO.add_event_detect(RTH1, GPIO.RISING, callback=event_right_rising)
-GPIO.add_event_detect(RTH1, GPIO.FALLING, callback=event_right_falling)
+GPIO.add_event_detect(RTH1, GPIO.BOTH, callback=event_right)
 
 
 def get_track_pos():
